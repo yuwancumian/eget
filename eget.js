e066e181079a9993204062e49beb91b088b8f799
easier <fysian@gmail.com>
2015-09-06 22:38:39 +0800

    static file

--- eget.js
+++ eget.js
@@ -9,23 +9,30 @@ var request = require('request');
     var name = process.argv[2];
     var version = process.argv[3];
     var opt = {
-        // 如果不指定版本就使用默认版本
-        jquery: '1.11.1',
-        'backbone.js': '1.1.2',
-        underscore: '1.7.0',
-        react: '0.12.0-rc1'
+        jquery: 'http://cdn.staticfile.org/jquery/1.11.1/jquery.js',
+        backbone: 'http://cdn.staticfile.org/backbone.js/1.1.2/backbone.js',
+        underscore: 'http://cdn.staticfile.org/underscore.js/1.7.0/underscore.js',
+        zepto: 'http://cdn.staticfile.org/zepto/1.1.4/zepto.js',
+        bootjs: 'http://cdn.staticfile.org/twitter-bootstrap/3.3.1/js/bootstrap.js',
+        bootcss: 'http://cdn.staticfile.org/twitter-bootstrap/3.3.1/css/bootstrap.css',
+        normalize: 'http://easier.b0.upaiyun.com/css/normalize.css',
+        modernizr: 'http://easier.b0.upaiyun.com/js/modernizr-2.6.2.min.js',
+        mustuche: 'http://cdn.staticfile.org/mustache.js/0.8.1/mustache.js',
+        require: 'http://cdn.bootcss.com/require.js/2.1.17/require.js',
+        ignore: 'http://easier.b0.upaiyun.com/.gitignore'
     };
     var address = 'http://cdn.staticfile.org/';
+    var filename = opt[name].split('/').pop();
     if (version) {
         (function( name ){
-            request(address + name +'/' + version +'/'+ name + '.js').pipe(fs.createWriteStream(name+'.js'));
+            request(address + name +'/' + version +'/'+ name + '.js').pipe(fs.createWriteStream(filename));
         })(name);
-        console.log("version is ok!");
+        console.log(filename +" was download!");
     } else {
         (function( name ){
-            request(address + name +'/' + opt[name] +'/'+ name + '.js').pipe(fs.createWriteStream(name+'.js'));
+            request(opt[name]).pipe(fs.createWriteStream(filename));
         })(name);
-        console.log(address + name +'/' + opt[name] +'/'+ name + '.js');
+        console.log(filename +" was download!");
     }
 
 })();
