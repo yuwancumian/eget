906308ed9606546576a9e43b94a8b557d69eacc2
yuwancumian <easier2005@163.com>
2015-11-27 17:31:20 +0800

    add cfg.json

--- getcdn.js
+++ getcdn.js
@@ -1,41 +1,39 @@
 #!/usr/bin/env node
 var request = require('request');
-var libs;
+var fs = require('fs');
+var cfg = require('./lib/cfg');
 
+var name = process.argv[2];
+var url = '';
 
 var getversion = function(name){
-    request('https://api.cdnjs.com/libraries',function(error,response,body){
-        if (!error && response.statusCode == 200) {
-            console.log(typeof(body)) // Show the HTML for the Google homepage.
-            libs = JSON.parse(body)['results'];
-            var start=new Date().getTime();
-            //console.log(start);
-            for(var key in libs){
-                var item =libs[key];
-                if(item['name']==name){
-                    console.log(item.latest);
-                }
-            }
-            var end = new Date().getTime();
-            console.log(end-start);
-            // var count=0;
-            // var timer = setInterval(function function_name() {
-            //     count++;
-            //
-            // },1);
+    request('https://api.cdnjs.com/libraries').pipe(fs.createWriteStream('./lib/cfg.json'));
+}
 
-            // clearInterval(timer);
-            // console.log(count);
-                   // console.log(libs);
+var libs = cfg.results;
+//console.log(libs);
+
+
+var getbaidu = function(){request('www.baidu.com', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body) // Show the HTML for the Google homepage.
+        } else{
+            console.log(error)
         }
     })
 }
+//getbaidu();
+//
+//getversion();
 
+for(key in libs){
+    //console.log(key);
+    var item = libs[key];
+    if (item['name'] === name) {
+        var url = item['latest'];
+        var remove =url.split('/').slice(3);
+        console.log(remove);
+        console.log("http://v2.easier.upyun.com/"+remove.join('/'))
+    }
+}
 
-/*request('www.baidu.com', function (error, response, body) {*/
-  //if (!error && response.statusCode == 200) {
-    //console.log(body) // Show the HTML for the Google homepage.
-  //}
-/*})*/
-
-getversion("zxcvbn");
