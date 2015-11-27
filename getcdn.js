50184539184f6385d1a2dee382c9d811fc142869
yuwancumian <easier2005@163.com>
2015-11-27 09:41:25 +0800

    gettimer

--- getcdn.js
+++ getcdn.js
@@ -3,20 +3,39 @@ var request = require('request');
 var libs;
 
 
-var getversion = function(){
+var getversion = function(name){
     request('https://api.cdnjs.com/libraries',function(error,response,body){
         if (!error && response.statusCode == 200) {
-            console.log(body) // Show the HTML for the Google homepage. 
-            libs = body[results];
-            console.log(libs);
+            console.log(typeof(body)) // Show the HTML for the Google homepage.
+            libs = JSON.parse(body)['results'];
+            var start=new Date().getTime();
+            //console.log(start);
+            for(var key in libs){
+                var item =libs[key];
+                if(item['name']==name){
+                    console.log(item.latest);
+                }
+            }
+            var end = new Date().getTime();
+            console.log(end-start);
+            // var count=0;
+            // var timer = setInterval(function function_name() {
+            //     count++;
+            //
+            // },1);
+
+            // clearInterval(timer);
+            // console.log(count);
+                   // console.log(libs);
         }
     })
 }
+
+
 /*request('www.baidu.com', function (error, response, body) {*/
   //if (!error && response.statusCode == 200) {
-    //console.log(body) // Show the HTML for the Google homepage. 
+    //console.log(body) // Show the HTML for the Google homepage.
   //}
 /*})*/
 
-getversion();
-
+getversion("zxcvbn");
