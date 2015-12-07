213c5ba442288638c1b4ffa8b89280ff80e86883
yuwancumian <easier2005@163.com>
2015-12-07 17:37:32 +0800

    rewrite

--- eget.js
+++ eget.js
@@ -5,33 +5,43 @@ var cfg = require('./config');
 var path = require('path');
 
 (function(){
-    var address = 'http://cdn.staticfile.org/';
-    var name = process.argv[2];
-    var url = process.argv[3];
-    var filename = cfg[name].split('/').pop();
-    
-    if (name === "set") {
+    var address;
+    var name;
+    var url;
+    var filename;
+
+    if (process.argv[2] === "set") {
+
         name = process.argv[3];
         url = process.argv[4];
         cfg[name] = url;
-        console.log(__dirname);
         fs.writeFile(path.join(__dirname,'config.json'),JSON.stringify(cfg,null,4),function(err){
             if (err) throw err;
             console.log(name +' was added!');
         })
-    } else {
 
+
+    } else {
+    
+        name = process.argv[2];
+        url = process.argv[3];
+        filename = cfg[name].split('/').pop();
+        
         if (cfg[name] === undefined) {
-            console.log( "Sorry, " + name + " was not uncached...");
-            return ;
+            
+          console.log( "Sorry, " + name + " was not uncached...");
+          return ;
+
         } else {
+                
             console.log(filename);
             (function( name ){
                 request(cfg[name]).pipe(fs.createWriteStream(filename));
             })(name);
-        }
+    
+            console.log(filename +" was download!");
 
+        }
     }
-    console.log(filename +" was download!");
-    cfg = require('./config');
+
 })();
